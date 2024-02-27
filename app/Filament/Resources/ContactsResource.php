33e119386beb990f3a3c88b544c87139c6c8e5dd
“fathia <toohanour@gmail.com>
2024-02-27 17:01:32 +0300

    contacts resource table issue fixed

--- app/Filament/Resources/ContactsResource.php
+++ app/Filament/Resources/ContactsResource.php
@@ -1,17 +1,19 @@
 <?php
-
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\ContactsResource\Pages;
-use App\Filament\Resources\ContactsResource\RelationManagers;
 use App\Models\Contacts;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+// use Filament\Tables\Columns;
+// use Filament\Tables\Rows;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+
 
 class ContactsResource extends Resource
 {
@@ -23,7 +25,11 @@ class ContactsResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('first_name')->label('First Name'),
+                Forms\Components\TextInput::make('last_name')->label('Last Name'),
+                Forms\Components\TextInput::make('email')->label('Email'),
+                Forms\Components\TextInput::make('phone')->label('Phone'),
+                Forms\Components\TextInput::make('job_title')->label('Job Title'),
             ]);
     }
 
@@ -31,26 +37,24 @@ class ContactsResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('first_name')->label('First Name'),
+                Tables\Columns\TextColumn::make('last_name')->label('Last Name'),
+                Tables\Columns\TextColumn::make('email')->label('Email'),
+                Tables\Columns\TextColumn::make('phone')->label('Phone'),
+                Tables\Columns\TextColumn::make('job_title')->label('Job Title'),
             ])
             ->filters([
                 //
             ])
+            ->defaultSort('id', 'asc') // You can change 'id' to any other column you want to sort by default
             ->actions([
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
     }
 
     public static function getPages(): array
